7f97981be45929d7c669e0479c5da54a14ba231c
weezzzt <wille1tap@outlook.com>
2024-03-27 00:01:37 +0100

    built the vite-app

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
